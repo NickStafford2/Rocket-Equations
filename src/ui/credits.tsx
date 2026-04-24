4ef353fed2ba7cec2c01be399e00378c2936b31f
= <ngstafford1@gmail.com>
2026-04-23 21:12:01 -0400

    added saturnV model

---
+++ src/ui/credits.tsx
@@ -0,0 +1,10 @@
+export function Credits() {
+  return (
+    <div>
+      <span>Nicholas Stafford</span>
+      <ul>
+        <li>Saturn V by Mr. Peel [CC-BY] via Poly Pizza</li>
+      </ul>
+    </div>
+  );
+}
