9f30eeaae1c2c411a259fc08067f3304509e7681
= <ngstafford1@gmail.com>
2026-05-09 04:52:13 -0400

    good looking tarkim prototype

--- src/three/takram/TakramAtmospherePrototype.tsx
+++ src/three/takram/TakramAtmospherePrototype.tsx
@@ -1,104 +1,293 @@
-import { OrbitControls, Stars } from "@react-three/drei";
-import { Canvas, useThree } from "@react-three/fiber";
-import { EffectComposer } from "@react-three/postprocessing";
+// TakramAtmosphereTest.tsx
+
+import { useEffect, useRef } from "react";
 import {
-  AerialPerspective,
-  Atmosphere,
-  Sky,
-} from "@takram/three-atmosphere/r3f";
-import { useEffect } from "react";
-import * as THREE from "three";
+  EffectComposer,
+  EffectPass,
+  RenderPass,
+  ToneMappingEffect,
+  ToneMappingMode,
+} from "postprocessing";
+import {
+  Group,
+  HalfFloatType,
+  Matrix4,
+  Mesh,
+  MeshPhysicalMaterial,
+  NoToneMapping,
+  PCFSoftShadowMap,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Points,
+  Scene,
+  Timer,
+  TorusKnotGeometry,
+  Vector3,
+  WebGLRenderer,
+  type Object3D,
+} from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-import { EARTH_RADIUS_METERS } from "./constants";
-import { EarthSurface } from "./EarthSurface";
+import {
+  AerialPerspectiveEffect,
+  getECIToECEFRotationMatrix,
+  getMoonDirectionECI,
+  getSunDirectionECI,
+  PrecomputedTexturesGenerator,
+  SkyLightProbe,
+  SkyMaterial,
+  StarsGeometry,
+  StarsMaterial,
+  SunDirectionalLight,
+} from "@takram/three-atmosphere";
+import {
+  ArrayBufferLoader,
+  Ellipsoid,
+  Geodetic,
+  radians,
+} from "@takram/three-geospatial";
+import {
+  DitheringEffect,
+  LensFlareEffect,
+} from "@takram/three-geospatial-effects";
 
-const FAR_SPACE_ALTITUDE_METERS = 20_000_000;
-const VERY_FAR_SPACE_ALTITUDE_METERS = 45_000_000;
+const referenceDate = new Date("2000-06-01T10:00:00Z");
+const geodetic = new Geodetic(0, radians(67), 1000);
+const position = geodetic.toECEF();
+const up = Ellipsoid.WGS84.getSurfaceNormal(position);
 
-const TEST_DATE = new Date("2026-06-21T16:00:00Z");
+export function TakramAtmospherePrototype() {
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
-type TakramAtmospherePrototypeProps = {
-  className?: string;
-};
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
 
-export function TakramAtmospherePrototype({
-  className,
-}: TakramAtmospherePrototypeProps) {
-  return (
-    <Canvas
-      className={className}
-      frameloop="always"
-      gl={{
-        antialias: true,
-        powerPreference: "high-performance",
+    let disposed = false;
+
+    const inertialToECEFMatrix = new Matrix4();
+    const sunDirection = new Vector3();
+    const moonDirection = new Vector3();
+
+    let renderer: WebGLRenderer;
+    let camera: PerspectiveCamera;
+    let controls: OrbitControls;
+    let timer: Timer;
+    let scene: Scene;
+    let skyMaterial: SkyMaterial;
+    // let starsMaterial: StarsMaterial;
+    // let stars: Object3D;
+    let skyLight: SkyLightProbe;
+    let sunLight: SunDirectionalLight;
+    let aerialPerspective: AerialPerspectiveEffect;
+    let composer: EffectComposer;
+
+    async function init() {
+      const width = container.clientWidth || window.innerWidth;
+      const height = container.clientHeight || window.innerHeight;
+      const aspect = width / height;
+
+      camera = new PerspectiveCamera(75, aspect, 10, 1e6);
+      camera.position.copy(position);
+      camera.up.copy(up);
+
+      controls = new OrbitControls(camera, container);
+      controls.enableDamping = true;
+      controls.minDistance = 1e3;
+      controls.target.copy(position);
+
+      timer = new Timer();
+      scene = new Scene();
+
+      sunLight = new SunDirectionalLight({ distance: 300 });
+      sunLight.target.position.copy(position);
+      sunLight.castShadow = true;
+      sunLight.shadow.camera.top = 300;
+      sunLight.shadow.camera.bottom = -300;
+      sunLight.shadow.camera.left = -300;
+      sunLight.shadow.camera.right = 300;
+      sunLight.shadow.camera.near = 0;
+      sunLight.shadow.camera.far = 600;
+      sunLight.shadow.mapSize.width = 2048;
+      sunLight.shadow.mapSize.height = 2048;
+      sunLight.shadow.normalBias = 1;
+
+      scene.add(sunLight);
+      scene.add(sunLight.target);
+
+      const group = new Group();
+
+      Ellipsoid.WGS84.getEastNorthUpFrame(position).decompose(
+        group.position,
+        group.quaternion,
+        group.scale,
+      );
+
+      scene.add(group);
+
+      const torusKnot = new Mesh(
+        new TorusKnotGeometry(200, 60, 256, 64),
+        new MeshPhysicalMaterial({
+          color: "white",
+          roughness: 0.5,
+          ior: 1.45,
+          clearcoat: 1,
+          clearcoatRoughness: 0.1,
+        }),
+      );
+
+      torusKnot.castShadow = true;
+      torusKnot.receiveShadow = true;
+      group.add(torusKnot);
+
+      // starsMaterial = new StarsMaterial();
+      // starsMaterial.intensity = 10;
+      //
+      // stars = new Points(
+      //   new StarsGeometry(
+      //     await new ArrayBufferLoader().loadAsync("/atmosphere/stars.bin"),
+      //   ),
+      //   starsMaterial,
+      // );
+      //
+      // stars.frustumCulled = false;
+      // scene.add(stars);
+
+      skyMaterial = new SkyMaterial();
+
+      const sky = new Mesh(new PlaneGeometry(2, 2), skyMaterial);
+      sky.frustumCulled = false;
+      scene.add(sky);
+
+      skyLight = new SkyLightProbe();
+      skyLight.position.copy(position);
+      scene.add(skyLight);
+
+      aerialPerspective = new AerialPerspectiveEffect(camera);
+
+      renderer = new WebGLRenderer({
+        depth: false,
         logarithmicDepthBuffer: true,
-      }}
-      camera={{
-        fov: 45,
-        near: 10,
-        far: 120_000_000,
-        position: [
-          EARTH_RADIUS_METERS + FAR_SPACE_ALTITUDE_METERS,
-          EARTH_RADIUS_METERS * 0.5,
-          EARTH_RADIUS_METERS * 1.5,
-        ],
-      }}
-    >
-      <RendererSettings />
-      <InitialCameraLookAt />
+      });
 
-      <color attach="background" args={["#000000"]} />
+      renderer.setPixelRatio(window.devicePixelRatio);
+      renderer.setSize(width, height);
+      renderer.toneMapping = NoToneMapping;
+      renderer.toneMappingExposure = 10;
+      renderer.shadowMap.enabled = true;
+      renderer.shadowMap.type = PCFSoftShadowMap;
 
-      <Stars
-        radius={90_000_000}
-        depth={20_000_000}
-        count={8000}
-        factor={8}
-        saturation={0}
-        fade
-        speed={0}
-      />
+      composer = new EffectComposer(renderer, {
+        frameBufferType: HalfFloatType,
+        multisampling: 8,
+      });
 
-      <Atmosphere date={TEST_DATE}>
-        <Sky />
-        <EarthSurface />
+      composer.addPass(new RenderPass(scene, camera));
+      composer.addPass(new EffectPass(camera, aerialPerspective));
+      composer.addPass(
+        new EffectPass(
+          camera,
+          new LensFlareEffect(),
+          new ToneMappingEffect({ mode: ToneMappingMode.AGX }),
+          new DitheringEffect(),
+        ),
+      );
 
-        <EffectComposer enableNormalPass>
-          <AerialPerspective sky sunLight skyLight />
-        </EffectComposer>
-      </Atmosphere>
+      const generator = new PrecomputedTexturesGenerator(renderer);
 
-      <OrbitControls
-        makeDefault
-        enableDamping
-        dampingFactor={0.08}
-        target={[0, 0, 0]}
-        minDistance={EARTH_RADIUS_METERS + 20}
-        maxDistance={EARTH_RADIUS_METERS + VERY_FAR_SPACE_ALTITUDE_METERS}
-      />
-    </Canvas>
-  );
-}
+      generator.update().catch((error: unknown) => {
+        console.error(error);
+      });
 
-function RendererSettings() {
-  const { gl } = useThree();
+      const { textures } = generator;
 
-  useEffect(() => {
-    gl.outputColorSpace = THREE.SRGBColorSpace;
-    gl.toneMapping = THREE.ACESFilmicToneMapping;
-    gl.toneMappingExposure = 1.15;
-  }, [gl]);
+      Object.assign(skyMaterial, textures);
+      sunLight.transmittanceTexture = textures.transmittanceTexture;
+      skyLight.irradianceTexture = textures.irradianceTexture;
+      // Object.assign(starsMaterial, textures);
+      Object.assign(aerialPerspective, textures);
 
-  return null;
-}
+      container.appendChild(renderer.domElement);
 
-function InitialCameraLookAt() {
-  const { camera } = useThree();
+      function onResize() {
+        const nextWidth = container.clientWidth || window.innerWidth;
+        const nextHeight = container.clientHeight || window.innerHeight;
 
-  useEffect(() => {
-    camera.lookAt(0, 0, 0);
-    camera.updateProjectionMatrix();
-  }, [camera]);
+        camera.aspect = nextWidth / nextHeight;
+        camera.updateProjectionMatrix();
 
-  return null;
+        renderer.setSize(nextWidth, nextHeight);
+        composer.setSize(nextWidth, nextHeight);
+      }
+
+      function render(time: number) {
+        if (disposed) return;
+
+        timer.update(time);
+
+        const date = +referenceDate + ((timer.getElapsed() * 5e6) % 864e5);
+
+        getECIToECEFRotationMatrix(date, inertialToECEFMatrix);
+
+        getSunDirectionECI(date, sunDirection).applyMatrix4(
+          inertialToECEFMatrix,
+        );
+
+        getMoonDirectionECI(date, moonDirection).applyMatrix4(
+          inertialToECEFMatrix,
+        );
+
+        skyMaterial.sunDirection.copy(sunDirection);
+        skyMaterial.moonDirection.copy(moonDirection);
+
+        // starsMaterial.sunDirection.copy(sunDirection);
+
+        sunLight.sunDirection.copy(sunDirection);
+        skyLight.sunDirection.copy(sunDirection);
+        aerialPerspective.sunDirection.copy(sunDirection);
+
+        // stars.setRotationFromMatrix(inertialToECEFMatrix);
+
+        sunLight.update();
+        skyLight.update();
+        controls.update();
+
+        composer.render();
+      }
+
+      window.addEventListener("resize", onResize);
+      renderer.setAnimationLoop(render);
+
+      onResize();
+
+      return () => {
+        window.removeEventListener("resize", onResize);
+      };
+    }
+
+    let cleanupResize: (() => void) | undefined;
+
+    void init().then((cleanup) => {
+      cleanupResize = cleanup;
+    });
+
+    return () => {
+      disposed = true;
+
+      cleanupResize?.();
+
+      if (renderer) {
+        renderer.setAnimationLoop(null);
+        renderer.dispose();
+
+        if (renderer.domElement.parentElement === container) {
+          container.removeChild(renderer.domElement);
+        }
+      }
+
+      controls?.dispose();
+    };
+  }, []);
+
+  return <div ref={containerRef} className="h-screen w-screen" />;
 }
